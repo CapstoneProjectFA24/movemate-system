2edc1915f51b6f0fae88bd9a9cf5c057e0badc20
cophuocvinh <cophuocvinh@gmail.com>
2024-10-17 14:34:57 +0700

    chore: update config redis for register booking

--- MoveMate.Service/ThirdPartyService/RabbitMQ/Worker/AssginReiewWorker.cs
+++ MoveMate.Service/ThirdPartyService/RabbitMQ/Worker/AssginReiewWorker.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using MoveMate.Repository.Repositories.UnitOfWork;
 using MoveMate.Service.ThirdPartyService.RabbitMQ.Annotation;
@@ -7,24 +8,35 @@ namespace MoveMate.Service.ThirdPartyService.RabbitMQ.Worker;
 
 public class AssginReiewWorker
 {
-    private UnitOfWork _unitOfWork;
-    private IMapper _mapper;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
     private readonly ILogger<AssginReiewWorker> _logger;
-    
-    public AssginReiewWorker(IUnitOfWork unitOfWork, IMapper mapper, ILogger<AssginReiewWorker> logger)
+
+    public AssginReiewWorker(IServiceScopeFactory serviceScopeFactory, ILogger<AssginReiewWorker> logger)
     {
-        _unitOfWork = (UnitOfWork)unitOfWork;
-        _mapper = mapper;
+        _serviceScopeFactory = serviceScopeFactory;
         _logger = logger;
     }
 
     [Consumer("movemate.booking_assign_review")]
-    public async void HandleMessage(int message)
+    public async Task HandleMessage(int message)
     {
-        var booking = await _unitOfWork.BookingRepository.GetByIdAsync(message);
+        try
+        {
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var unitOfWork =(UnitOfWork) scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+                var mapper = scope.ServiceProvider.GetRequiredService<IMapper>();
 
+                var booking = await unitOfWork.BookingRepository.GetByIdAsync(message);
+                Console.WriteLine($"Booking info: {booking}");
 
-        // Xử lý thông điệp ở đây
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error processing booking review for message {Message}", message);
+            throw;
+        }
         Console.WriteLine($"Received message: {message}");
     }
 }
