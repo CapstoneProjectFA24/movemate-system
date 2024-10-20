ea389584c5011f538061e664b50c5a9979fd08fd
cophuocvinh <cophuocvinh@gmail.com>
2024-10-20 21:53:03 +0700

    chore: update  valuation  booking

--- MoveMate.Service/ViewModels/ModelResponses/Booking/BookingValuationResponse.cs
+++ MoveMate.Service/ViewModels/ModelResponses/Booking/BookingValuationResponse.cs
@@ -3,6 +3,7 @@
 public class BookingValuationResponse
 {
     public double Total { get; set; }
+    public double Deposit { get; set; }
 
     public virtual List<ServiceDetailsResponse> ServiceDetails { get; set; } = new List<ServiceDetailsResponse>();
     public virtual ICollection<FeeDetailResponse> FeeDetails { get; set; } = new List<FeeDetailResponse>();
