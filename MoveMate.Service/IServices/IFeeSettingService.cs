3a0bd97f8c5e4940e1a1bea5a694c0055bba4cc2
lehananh0606 <lehananh06@gmail.com>
2024-10-31 16:33:10 +0700

    feat: CRUD fee setting

---
+++ MoveMate.Service/IServices/IFeeSettingService.cs
@@ -0,0 +1,20 @@
+﻿using MoveMate.Service.Commons;
+using MoveMate.Service.ViewModels.ModelRequests;
+using MoveMate.Service.ViewModels.ModelResponses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveMate.Service.IServices
+{
+    public interface IFeeSettingService
+    {
+        public Task<OperationResult<List<PromotionResponse>>> GetAllFeeSetting(GetAllPromotionRequest request);
+        public Task<OperationResult<PromotionResponse>> GetFeeSettingById(int id);
+        Task<OperationResult<PromotionResponse>> UpdateFeeSetting(int id, UpdatePromotionRequest request);
+        Task<OperationResult<PromotionResponse>> CreateFeeSetting(CreatePromotionRequest request);
+        Task<OperationResult<bool>> DeleteFeeSetting(int id);
+    }
+}
---
+++ MoveMate.Service/ViewModels/ModelRequests/GetAllFeeSettingRequest.cs
@@ -0,0 +1,44 @@
+﻿using LinqKit;
+using MoveMate.Domain.Models;
+using MoveMate.Service.Commons.Page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveMate.Service.ViewModels.ModelRequests
+{
+    public class GetAllFeeSettingRequest : PaginationRequestV2<FeeSetting>
+    {
+        public string? Search { get; set; }
+        public int? ServiceId { get; set; }
+
+        public override Expression<Func<FeeSetting, bool>> GetExpressions()
+        {
+            if (!string.IsNullOrWhiteSpace(Search))
+            {
+                Search = Search.Trim().ToLower();
+
+                var queryExpression = PredicateBuilder.New<FeeSetting>(true);
+                queryExpression.Or(cus => cus.Name.ToLower().Contains(Search));
+                queryExpression.Or(cus => cus.Description.ToLower().Contains(Search));
+
+                Expression = Expression.And(queryExpression);
+            }
+
+            if (ServiceId.HasValue)
+            {
+                Expression = Expression.And(u => u.ServiceId == ServiceId.Value);
+            }
+
+           
+
+            Expression = Expression.And(u => u.IsActived == true);
+
+            return Expression;
+        }
+
+    }
+}
