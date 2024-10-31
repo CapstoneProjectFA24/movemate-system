6a16c7346d983935fc457a523ccb64947e994e07
lehananh0606 <lehananh06@gmail.com>
2024-10-31 22:29:43 +0700

    chore: api fee setting

--- MoveMate.API/Controllers/FeeController.cs
+++ MoveMate.API/Controllers/FeeController.cs
@@ -35,5 +35,38 @@ namespace MoveMate.API.Controllers
 
             return response.IsError ? HandleErrorResponse(response.Errors) : Ok(response);
         }
+
+        /// <summary>
+        /// CHORE : Retrieves a fee setting by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the fee setting to retrieve.</param>
+        /// <returns>An IActionResult containing the operation result.</returns>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     GET /feesetting/1
+        /// </remarks>
+        /// <response code="200">Get fee setting success</response>
+        /// <response code="404">Fee setting not found</response>
+        /// <response code="500">Internal server error occurred</response>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFeeSettingById(int id)
+        {
+            var response = await _feeSettingServices.GetFeeSettingById(id);
+            return response.IsError ? HandleErrorResponse(response.Errors) : Ok(response);
+        }
+
+        /// <summary>
+        /// CHORE : Set fee setting's IsActive become false by fee setting Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [Authorize]
+        public async Task<IActionResult> DeleteFeeSettingById(int id)
+        {
+            var response = await _feeSettingServices.DeleteActiveFeeSetting(id);
+            return response.IsError ? HandleErrorResponse(response.Errors) : Ok(response);
+        }
     }
 }
