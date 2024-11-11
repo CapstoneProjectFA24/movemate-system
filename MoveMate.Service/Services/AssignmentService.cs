93bdc49c900ee20365361ee48453089fff69efae
cophuocvinh <cophuocvinh@gmail.com>
2024-11-12 01:15:08 +0700

    chore: chote

---
+++ MoveMate.Service/Services/AssignmentService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using MoveMate.Repository.Repositories.UnitOfWork;
+using MoveMate.Service.ThirdPartyService.Firebase;
+using MoveMate.Service.ThirdPartyService.RabbitMQ;
+
+namespace MoveMate.Service.Services;
+
+public class AssignmentService
+{
+    private UnitOfWork _unitOfWork;
+    private IMapper _mapper;
+    private readonly ILogger<AssignmentService> _logger;
+    private readonly IMessageProducer _producer;
+    private readonly IFirebaseServices _firebaseServices;
+
+    public AssignmentService(UnitOfWork unitOfWork, IMapper mapper, ILogger<AssignmentService> logger, IMessageProducer producer, IFirebaseServices firebaseServices)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+        _logger = logger;
+        _producer = producer;
+        _firebaseServices = firebaseServices;
+    }
+    
+    
+    
+}
